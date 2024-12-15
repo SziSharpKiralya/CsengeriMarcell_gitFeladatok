84fc6a4394a4a8acafbfda0aa76fa9b46ecd3c6c
matat <matat@DESKTOP-DTAHLRI>
2024-12-15 14:21:30 +0100

    Add project files.

---
+++ Elso_Feladatok/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.SymbolStore;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Elso_Feladatok
+{
+    internal class Program
+    {
+        static void F01(int[] tomb)
+        {
+            Random random = new Random();
+            for (int i = 0; i < tomb.Length; i++)
+            {
+                tomb[i] = random.Next(1, 51);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            F01(new int[10]);
+        }
+    }
+}
